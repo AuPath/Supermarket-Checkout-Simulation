550acc0661d63cbbd7dc539225876ede9a8477c8
lucreziatonelli <lucreziatonelli17@gmail.com>
2021-07-06 14:10:21 +0200

    Grid supermarket

--- src/Supermarket.py
+++ src/Supermarket.py
@@ -1,13 +1,16 @@
 from Customer import Customer
+from mesa import Model
 from src.queue.SupermarketQueue import SupermarketQueue
+from mesa.space import MultiGrid
 
 ADJ_WINDOW_SIZE = 2
 
 
-class Supermarket:
-    def __init__(self, customers: set[Customer], queues: list[SupermarketQueue], adj_window_size=ADJ_WINDOW_SIZE):
+class Supermarket(Model):
+    def __init__(self, customers: set[Customer], queues: list[SupermarketQueue], width, height, adj_window_size=ADJ_WINDOW_SIZE):
         self.__customers = customers
         self.__queues = queues
+        self.grid = MultiGrid(width, height, True)
 
     def add_customer(self, customer: Customer):
         self.__customers.add(customer)
