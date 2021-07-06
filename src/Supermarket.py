3036c00f5718e3fb2e6a536889344d9d5781dc2a
gianlucagiudice <gianlu.giudice@gmail.com>
2021-07-06 14:21:29 +0200

    Empty customers

--- src/Supermarket.py
+++ src/Supermarket.py
@@ -7,10 +7,11 @@ ADJ_WINDOW_SIZE = 2
 
 
 class Supermarket(Model):
-    def __init__(self, customers: set[Customer], queues: list[SupermarketQueue], width, height, adj_window_size=ADJ_WINDOW_SIZE):
-        self.__customers = customers
+    def __init__(self, queues: list[SupermarketQueue], adj_window_size=ADJ_WINDOW_SIZE):
+        self.__customers = set()
         self.__queues = queues
-        self.grid = MultiGrid(width, height, True)
+        self.__grid = MultiGrid(False)
+        self.__adj_window_size = adj_window_size
 
     def add_customer(self, customer: Customer):
         self.__customers.add(customer)
