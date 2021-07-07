001e09c955341c43aab7d06da28fd211a2377fc9
AuPath <marco.au.grassi98@protonmail.com>
2021-07-07 17:58:44 +0200

    concrete least items in queue choice strategy.

---
+++ src/queuechoicestrategy/QueueChoiceLeastItems.py
@@ -0,0 +1,18 @@
+from operator import methodcaller
+import math
+
+class QueueChoiceLeastItems(QueueChoiceStrategy):
+
+    # Returns shortest/smalles queue from queues, the comparison is made on the
+    # basis of the size() function of the queues
+    def choose_queue(self, queues: List):
+        min = math.inf
+        chosen_queue = None
+
+        # Find queue with least items
+        for q in queues:
+            if q.total_items() < min:
+                chosen_queue = q
+            
+        return chosen_queue
+
