6578b44b3c9d55f1d75d59ef2e35b20a255a2a6b
AuPath <marco.au.grassi98@protonmail.com>
2021-07-07 17:35:26 +0200

    contains and content utility methods to SuperMarketQueue class.

--- src/queue/SupermarketQueue.py
+++ src/queue/SupermarketQueue.py
@@ -4,7 +4,6 @@ from queue import Queue
 from mesa import Agent
 from src.Customer import Customer
 
-
 class SupermarketQueue(ABC, Agent):
 
     @abstractmethod
@@ -22,3 +21,22 @@ class SupermarketQueue(ABC, Agent):
     @abstractmethod
     def size(self):
         return self.__queue.qsize()
+
+    # True if c is in the queue, False otherwise
+    @abstractmethod
+    def contains(self, c:Customer):
+
+        for x in self.content(self):
+            if x == c:
+                return True
+        return False    
+
+    # Returns an ordered list of the Customers in the queue
+    @abstractmethod
+    def content(self):
+        l = []
+
+        while self.__queue.qsize() > 0:
+            l.append(self.__queue.get())
+
+        return l
