7545ba6050c802729a0b22875dbb6aa4b6ca7212
gianlucagiudice <gianlu.giudice@gmail.com>
2021-07-08 00:34:06 +0200

    Abstract decorator removed due to python error

--- src/queue/NormalQueue.py
+++ src/queue/NormalQueue.py
@@ -1,18 +1,7 @@
-from src.Customer import Customer
 from src.queue.SupermarketQueue import SupermarketQueue
-from queue import Queue
 
 
 class NormalQueue(SupermarketQueue):
 
     def __init__(self):
-        self.__queue = Queue()
-
-    def enqueue(self, c: Customer):
-        super.enqueue()
-
-    def dequeue(self):
-        return super.dequeue()
-
-    def size(self):
-        return super.size()
+        super().__init__()
--- src/queue/SupermarketQueue.py
+++ src/queue/SupermarketQueue.py
@@ -4,35 +4,29 @@ from queue import Queue
 from mesa import Agent
 from src.Customer import Customer
 
+
 class SupermarketQueue(ABC, Agent):
 
     @abstractmethod
     def __init__(self):
         self.__queue = Queue()
 
-    @abstractmethod
-    def enqueue(self, c:Customer):
+    def enqueue(self, c: Customer):
         self.__queue.put(c)
 
-    @abstractmethod
     def dequeue(self):
         return self.__queue.get()
 
-    @abstractmethod
     def size(self):
         return self.__queue.qsize()
 
-    # True if c is in the queue, False otherwise
-    @abstractmethod
-    def contains(self, c:Customer):
+    def contains(self, c: Customer):
 
-        for x in self.content(self):
+        for x in self.content():
             if x == c:
                 return True
         return False    
 
-    # Returns an ordered list of the Customers in the queue
-    @abstractmethod
     def content(self):
         l = []
 
@@ -41,7 +35,6 @@ class SupermarketQueue(ABC, Agent):
 
         return l
 
-    @abstractmethod
     def total_items(self):
         bskt_size = 0
         for c in self.content():
