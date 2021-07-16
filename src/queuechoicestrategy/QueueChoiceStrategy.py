5a901c669381cf914ac47fdb399508623fbb12a6
gianlucagiudice <gianlu.giudice@gmail.com>
2021-07-16 16:39:16 +0200

    Scusate ma non mi funziona con le annotazioni

--- src/queuechoicestrategy/QueueChoiceStrategy.py
+++ src/queuechoicestrategy/QueueChoiceStrategy.py
@@ -1,6 +1,4 @@
 from abc import ABC, abstractmethod
-from src.cashdesk import CashDesk
-
 
 
 class QueueChoiceStrategy(ABC):
@@ -8,5 +6,5 @@ class QueueChoiceStrategy(ABC):
     # Must return a single queue
     # Assumo che le cash desk escludano quella self scan
     @abstractmethod
-    def choose_queue(self, cash_desks: list[CashDesk]):
+    def choose_queue(self, cash_desks):
         pass
