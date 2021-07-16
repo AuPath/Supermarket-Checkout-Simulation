29f3f4c095081c0f67b649b98d28259c159d4e28
AuPath <marco.au.grassi98@protonmail.com>
2021-07-16 15:17:40 +0200

    Correzione Strategy scelta coda.
    Per i criteri di scelta della coda del paper 3,4 bisogna per forza
    essere in grado di poter separare le code in base a che tipo di coda
    fa riferimento (Self service o Standard). Quindi ha piú senso che alla
    scelta delle coda venga passata la lista delle casse (assumendo che la
    self scan non sia tra queste perché questo tipo di cassa viene scelta
    al momento che il cliente entra nel negozio, non quando ha finito di
    fare la spesa come le altre). Dalle casse é possibile risalire alle
    code, il viceversa non é vero. La strategy ritorna comunque la coda
    piú conveniente.

--- src/queuechoicestrategy/QueueChoiceLeastItems.py
+++ src/queuechoicestrategy/QueueChoiceLeastItems.py
@@ -1,20 +1,25 @@
-from operator import methodcaller
 import math
-
 from src.queuechoicestrategy.QueueChoiceStrategy import QueueChoiceStrategy
+from src.cashdesk import CashDesk
 
 
 class QueueChoiceLeastItems(QueueChoiceStrategy):
 
-    # Returns shortest/smalles queue from queues, the comparison is made on the
+    # Returns shortest/smallest queue from queues, the comparison is made on the
     # basis of the size() function of the queues
-    def choose_queue(self, queues: list):
-        min = math.inf
+    def choose_queue(self, cash_desks: list[CashDesk]):
+
+        queues = []
+        for c in cash_desks:
+            queues.append(c.queue)
+
+        minimum = math.inf
         chosen_queue = None
 
         # Find queue with least items
         for q in queues:
-            if q.total_items() < min:
+            if q.total_items() < minimum:
+                minimum = q.total_items()
                 chosen_queue = q
             
         return chosen_queue
--- src/queuechoicestrategy/QueueChoiceStrategy.py
+++ src/queuechoicestrategy/QueueChoiceStrategy.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
+from src.cashdesk import CashDesk
+
 
 class QueueChoiceStrategy(ABC):
 
-    # Must return a single list
+    # Must return a single queue
+    # Assumo che le cash desk escludano quella self scan
     @abstractmethod
-    def choose_queue(self, queues: list):
+    def choose_queue(self, cash_desks: list[CashDesk]):
         pass
