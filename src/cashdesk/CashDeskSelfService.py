c95a48770d641114df00859d7a687be7f4a2dd88
AuPath <marco.au.grassi98@protonmail.com>
2021-07-16 15:35:18 +0200

    Metodi per il calcolo del service time per le casse CashDesk.
    Metodi per il calcolo di transaction time, break time e service time,
    cambiano i parametri a,b a seconda del tipo di cassa.

--- src/cashdesk/CashDeskSelfService.py
+++ src/cashdesk/CashDeskSelfService.py
@@ -1,7 +1,29 @@
 from src.cashdesk.CashDesk import CashDesk
 from src.queue.SupermarketQueue import SupermarketQueue
+from src.Customer import Customer
+import math
 
 
 class CashDeskSelfService(CashDesk):
+
     def __init__(self, agent_id, model, supermarket_queue: SupermarketQueue):
         super().__init__(agent_id, model, supermarket_queue)
+
+    def transaction_time(self, c: Customer):
+        a = 0.6725
+        b = 3.1223
+        return math.exp(a * math.log(c.basket_size) + b)
+
+    def break_time(self, c: Customer):
+        a = 0.2251
+        b = 3.5167
+        return math.exp(a * math.log(c.basket_size) + b)
+
+    def service_time(self, c: Customer):
+        return self.transaction_time(c) + self.break_time(c)
+
+    def service_time_total(self):
+        total = 0
+        for c in self.queue.content():
+            total += self.service_time(c)
+        return total
--- src/cashdesk/CashDeskStandard.py
+++ src/cashdesk/CashDeskStandard.py
@@ -1,7 +1,29 @@
 from src.cashdesk.CashDesk import CashDesk
 from src.queue.SupermarketQueue import SupermarketQueue
+from src.Customer import Customer
+import math
 
 
 class CashDeskStandard(CashDesk):
+
     def __init__(self, agent_id, model, supermarket_queue: SupermarketQueue):
         super().__init__(agent_id, model, supermarket_queue)
+
+    def transaction_time(self, c: Customer):
+        a = 0.6984
+        b = 2.1219
+        return math.exp(a * math.log(c.basket_size) + b)
+
+    def break_time(self, c: Customer): # Todo non la calcola cosi, la lascio cosí perché almeno si puó testare
+        a = 0.2251
+        b = 3.5167
+        return math.exp(a * math.log(c.basket_size) + b)
+
+    def service_time(self, c: Customer):
+        return self.transaction_time(c) + self.break_time(c)
+
+    def service_time_total(self):
+        total = 0
+        for c in self.queue.content():
+            total += self.service_time(c)
+        return total
