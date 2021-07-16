257abcb6807673dacdab6fc8533693d7f7e75263
AuPath <marco.au.grassi98@protonmail.com>
2021-07-16 17:00:42 +0200

    Calcolo corretto del break time per CashDeskStandard.
     Il calcolo del breaktime nel caso di cassa Standard non dipende da
     basket size. Nel paper non dice come lo calcola.  Andando a vedere
     nel codice netlogo lo calcola con una distribuzione gamma con i
     parametri usati in questa funzione. Non dice da dove prende questi
     parametri.  Viene lasciato Customer nella firma del metodo per non
     far sbagliare l'override di questo metodo.
     https://github.com/tant2002/NetLogo-Supermarket-Queue-Model/blob/master/SupermarketQueueModel_version_3_2.nlogo
     riga 689 Documentazione funzione: https://github.com/AFMac/rngs, il
     parametro che qui chiama lambda dovrebbe corrispondere a quello che
     python e wikipedia chiamano Beta

--- src/cashdesk/CashDeskStandard.py
+++ src/cashdesk/CashDeskStandard.py
@@ -1,5 +1,9 @@
+import random
+
 from src.cashdesk.CashDesk import CashDesk
 from src.queue.SupermarketQueue import SupermarketQueue
+from src.Customer import Customer
+
 
 class CashDeskStandard(CashDesk):
 
@@ -10,3 +14,17 @@ class CashDeskStandard(CashDesk):
         self.a_break = 0.2251
         self.b_break = 3.5167
 
+    # Il calcolo del breaktime nel caso di cassa Standard non dipende da
+    # basket size. Nel paper non dice come lo calcola.
+    # Andando a vedere nel codice netlogo lo calcola con una distribuzione gamma
+    # con i parametri usati in questa funzione. Non dice da dove prende questi parametri.
+    # Viene lasciato Customer nella firma del metodo per non far sbagliare l'override di questo metodo.
+    # https://github.com/tant2002/NetLogo-Supermarket-Queue-Model/blob/master/SupermarketQueueModel_version_3_2.nlogo
+    # riga 689
+    # Documentazione funzione: https://github.com/AFMac/rngs, il parametro che qui chiama lambda dovrebbe corrispondere a
+    # quello che python e wikipedia chiamano Beta
+
+    def break_time(self, c: Customer):
+        alpha = 3.074209
+        beta = 1 / 4.830613
+        return random.gammavariate(alpha, beta)
