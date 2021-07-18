39cd418cf3bac8952d9d22385ab015ce407e259b
AuPath <marco.au.grassi98@protonmail.com>
2021-07-18 11:56:58 +0200

    logica di jockeying nello stato CustomerQueuedState.

--- src/states/CustomerQueuedState.py
+++ src/states/CustomerQueuedState.py
@@ -5,5 +5,9 @@ from src.states.CustomerAtCashDeskState import CustomerAtCashDeskState
 class CustomerQueuedState(State):
 
     def action(self):
-        self.context.jockey()
-        # todo logica del jockeying
+        chosen_queue = self.context.jockey()
+
+        # Il controllo del None serve nel caso non vogliamo jockeying
+        if chosen_queue is not None and chosen_queue != self.context.target_queue:
+            self.context.target_queue = chosen_queue
+            self.context.target_queue.enqueue(self)
