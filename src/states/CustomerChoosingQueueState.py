d5a869e92f25477efffa85dcf8c1e33b8c6c5bb8
AuPath <marco.au.grassi98@protonmail.com>
2021-07-18 11:26:55 +0200

    refactoring CustomerChoosingQueueState.

--- src/states/CustomerChoosingQueueState.py
+++ src/states/CustomerChoosingQueueState.py
@@ -1,9 +1,17 @@
 from src.states.State import State
 from src.states.CustomerQueuedState import CustomerQueuedState
+import logging
 
 
 class CustomerChoosingQueueState(State):
 
     def action(self):
-        self.context.choose_queue()
+        logging.info("Customer " + str(self.context.unique_id) + " choosing queue")
+
+        self.context.target_queue = self.context.choose_queue()
+        self.context.target_queue.enqueue(self)
+
+        logging.info("Customer " + str(self.context.unique_id) + " moving to queue")
+        self.context.move_to_queue()
+
         self.context.state_change(CustomerQueuedState(self.context))
