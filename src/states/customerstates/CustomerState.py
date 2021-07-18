476b70d1c5871b3c941136dc61a5326bdf2f01c2
AuPath <marco.au.grassi98@protonmail.com>
2021-07-18 14:52:48 +0200

    Classe apposita per lo un CustomerState.

--- src/states/customerstates/CustomerState.py
+++ src/states/customerstates/CustomerState.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+from src.states.State import State
 from src.Customer import Customer
 
 
-class State(ABC):
+class CustomerState(State, ABC):
 
-    def __init__(self, c):
+    def __init__(self, c: Customer):
         self.context = c
 
     @abstractmethod
