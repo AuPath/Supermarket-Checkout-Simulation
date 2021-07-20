ab4a458a049d00c62d287b7bc1d5dfff5650d962
gianlucagiudice <gianlu.giudice@gmail.com>
2021-07-20 16:27:06 +0200

    Come vuoi

--- src/states/customerstates/CustomerQueuedState.py
+++ src/states/customerstates/CustomerQueuedState.py
@@ -13,8 +13,23 @@ class CustomerQueuedState(State):
 
             # Il controllo del None serve nel caso non vogliamo jockeying
             if chosen_queue is not None and chosen_queue != self.context.target_queue:
-                self.context.leave_queue() # todo logicamente lo fa nella coda, non so se peró lo bisogna fare anche a livello di cella
+
+                logging.info("Customer " + str(self.context.unique_id) +
+                             " is JOCKEYING from cashdesk " + str(self.context.get_cash_desk(self.context.target_queue).unique_id) +
+                             " to cashdesk " + str(self.context.get_cash_desk(chosen_queue).unique_id))
+
+
+                queue_before_customer = self.context.target_queue.content()
+                i = queue_before_customer.index(self.context)
+                queue_before_customer = queue_before_customer[i + 1:]
+
+                self.context.leave_queue()
 
                 self.context.target_queue = chosen_queue
                 self.context.target_queue.enqueue(self.context)
-                self.context.move_to_queue() # todo questo svuota la cella in cui era e riempie quella di destinazione ?
+                self.context.move_to_queue()
+
+                if len(queue_before_customer):
+                    for customer in queue_before_customer:
+                        self.context.get_cash_desk(self.context.target_queue).advance(customer)
+
