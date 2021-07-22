0903918576167ed952883db023d6f02aefd16161
lucreziatonelli <lucreziatonelli17@gmail.com>
2021-07-22 17:21:14 +0200

    Entrata continua di clienti

--- src/main.py
+++ src/main.py
@@ -1,22 +1,12 @@
 import logging
 from datetime import datetime
 
-from numpy import random
-import pandas as pd
-import math
-
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid
 
 from src.Supermarket import Supermarket
 from src.queuechoicestrategy.QueueChoiceLeastPeople import QueueChoiceLeastPeople
-from src.queuechoicestrategy.QueueChoiceLeastItems import QueueChoiceLeastItems
-from src.queuechoicestrategy.QueueChoiceLeastWaitingPowerImplied import QueueChoiceLeastWaitingPowerImplied
-from src.queuechoicestrategy.QueueChoiceLeastWaitingTimeServiceImplied import QueueChoiceLeastWaitingTimeServiceImplied
-
 from src.queuejockeystrategy.QueueJockeyLeastPeople import QueueJockeyLeastPeople
-from src.queuejockeystrategy.QueueJockeyNoJockeying import QueueJockeyNoJockeying
-from src.queuejockeystrategy.QueueJockeyLeastItems import QueueJockeyLeastItems
 
 # colors
 RED = "#eb3461"
@@ -26,9 +16,6 @@ GREEN = "#77eb34"
 ORANGE = "#eba234"
 GREY = "#a1a3a0"
 BLACK = "#000000"
-
-# Basket size exponential distribution parameter
-LAMBA_EXPONENTIAL_DISTRIBUTION = 0.0736184654254411
 
 filename = '../log/log' + str(datetime.now().strftime("%d-%m-%Y")) + '.log'
 logging.basicConfig(
@@ -72,29 +59,11 @@ def agent_portrayal(agent):
     return portrayal
 
 
-def generate_basket_size(n, lambda_parameter=LAMBA_EXPONENTIAL_DISTRIBUTION):
-    values = map(lambda x: math.ceil(x), random.exponential(scale=1 / lambda_parameter, size=n))
-    return list(values)
-
-
-def generate_customers_metadata(n_customers):
-    customers_metadata = []
-
-    basket_size_values = generate_basket_size(n_customers)
-
-    self_scan = False
-    for basket_size in basket_size_values:
-        # self_scan = not self_scan
-        new_tuple = (basket_size, self_scan, queue_choice_strategy, queue_jockeying_strategy)
-        customers_metadata.append(new_tuple)
-    return customers_metadata
-
-
 # Zones metadata
 entering_zone_width = 6
 shopping_zone_height = 3
 number_cash_desk_self_scan = 0
-number_cash_desk = 0
+number_cash_desk = 10
 
 number_cash_desk_self_service_groups = 1
 shared_queue = False  # coda unica tipo decathlon
@@ -106,12 +75,8 @@ zones_metadata = [('ENTERING', entering_zone_width),
                   ('CASH_DESK_SELF_SERVICE', number_cash_desk_self_service_groups),
                   ('CASH_DESK_RESERVED', 1)]
 
-# Customers metadata
-queue_choice_strategy = QueueChoiceLeastPeople()
-queue_jockeying_strategy = QueueJockeyLeastPeople()
-
-N_CUSTOMERS = 35
-customers_metadata = generate_customers_metadata(N_CUSTOMERS)
+# TODO: quanto vale uno step? qui 1 minuto, gli faccio entrare un cliente al minuto, e la giornata dura 8 ore
+customer_distribution = [1] * (60 * 8)
 
 height = 20
 # numero casse self-scan (ognuna occupa 2) + 1 spazio + barriera (1) + 1 spazio +
@@ -128,7 +93,11 @@ grid = CanvasGrid(agent_portrayal, width,
 server = ModularServer(Supermarket,
                        [grid],
                        "Supermarket",
-                       {"customers_metadata": customers_metadata,
-                        "zones_metadata": zones_metadata})
+                       {"zones_metadata": zones_metadata,
+                        # lista con numero di clienti da generare ad ogni step es. [1, 2, 3, 2, 4, 5, 2, ...]
+                        "customer_distribution": customer_distribution,
+                        "queue_choice_strategy": QueueChoiceLeastPeople(),
+                        "queue_jockey_strategy": QueueJockeyLeastPeople()
+                        })
 server.port = 8521  # The default
 server.launch()
