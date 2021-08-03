05757cfb96442f8d98eeed373a845b7226fe7bbd
lucreziatonelli <lucreziatonelli17@gmail.com>
2021-08-03 15:05:39 +0200

    Efficienza estrazione random nel jockey

--- src/queuejockeystrategy/QueueJockeyLeastItems.py
+++ src/queuejockeystrategy/QueueJockeyLeastItems.py
@@ -1,6 +1,7 @@
-from functools import reduce
 from operator import methodcaller
+
 from numpy import random
+
 from src.queuejockeystrategy.QueueJockeyStrategy import QueueJockeyStrategy
 
 
@@ -8,29 +9,33 @@ class QueueJockeyLeastItems(QueueJockeyStrategy):
 
     def __init__(self, threshold, prob_jockey):
         super().__init__()
-        self.prob_jockey = prob_jockey # Probabilità di fare jockey secondo bernoulli
+        self.prob_jockey = prob_jockey  # Probabilità di fare jockey secondo bernoulli
         self.threshold = threshold
 
     def switch_queue(self, customer, other_cash_desks):
 
         current_cash_desk = customer.get_cash_desk(customer.target_queue)
 
-        customers_in_queue = customer.target_queue.content()
-        customer_pos = customers_in_queue.index(customer)
-        customers_before_customer = customers_in_queue[:customer_pos]
+        if random.random() <= self.prob_jockey:
 
-        if current_cash_desk.customer is not None:
-            customers_before_customer.append(current_cash_desk.customer)
+            customers_in_queue = customer.target_queue.content()
+            customer_pos = customers_in_queue.index(customer)
+            customers_before_customer = customers_in_queue[:customer_pos]
 
-        current_queue_total_items = 0
-        for c in customers_before_customer:
-            current_queue_total_items += c.basket_size
+            if current_cash_desk.customer is not None:
+                customers_before_customer.append(current_cash_desk.customer)
 
-        min_other_cash_desks = min(other_cash_desks, key=methodcaller('total_items'))
+            current_queue_total_items = 0
+            for c in customers_before_customer:
+                current_queue_total_items += c.basket_size
 
-        queue_diff = current_queue_total_items - min_other_cash_desks.total_items()
+            min_other_cash_desks = min(other_cash_desks, key=methodcaller('total_items'))
 
-        if queue_diff > self.threshold and random.random() <= self.prob_jockey:
-            return min_other_cash_desks
+            queue_diff = current_queue_total_items - min_other_cash_desks.total_items()
+
+            if queue_diff > self.threshold:
+                return min_other_cash_desks
+            else:
+                return current_cash_desk
         else:
             return current_cash_desk
