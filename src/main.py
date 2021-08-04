496fb1de5e5c0bd5d9ace64b31957641d97a46fc
gianlucagiudice <gianlu.giudice@gmail.com>
2021-08-04 20:21:54 +0200

    Refactoring

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.modules import CanvasGrid, TextElement, ChartModule
+from mesa.visualization.modules import CanvasGrid, TextElement
 
 from src.Supermarket import Supermarket
 from src.queuechoicestrategy.QueueChoiceLeastPeople import QueueChoiceLeastPeople
@@ -20,6 +20,10 @@ class CashDeskLegendElement(TextElement):
 # Init log
 init_logging(LOG_PATH, enable_logging=False)
 
+# Init charts
+N_CHARTS = 1
+charts = init_charts()
+
 # Zones metadata
 entering_zone_width = 6
 shopping_zone_height = 10
@@ -32,76 +36,25 @@ probability_of_jockeying = 1
 
 number_cash_desk_self_service_groups = 1
 shared_queue = False  # coda unica tipo decathlon
-zones_metadata = [('ENTERING', entering_zone_width),
-                  ('SHOPPING', shopping_zone_height),
-                  ('CASH_DESK_SELF_SCAN', number_cash_desk_self_scan),
-                  (('CASH_DESK_STANDARD_SHARED_QUEUE', number_cash_desk) if shared_queue else
-                   ('CASH_DESK_STANDARD', number_cash_desk)),
-                  ('CASH_DESK_SELF_SERVICE', number_cash_desk_self_service_groups),
-                  ('CASH_DESK_RESERVED', 1)]
+
+zones_metadata = init_zones_metadata(
+    entering_zone_width, shopping_zone_height, number_cash_desk_self_scan,
+    number_cash_desk, shared_queue, number_cash_desk_self_service_groups)
 
 # TODO: quanto vale uno step? qui 1 minuto, gli faccio entrare un cliente al minuto, e la giornata dura 8 ore
 # TODO: mettere la distribuzione del Dottor Giudice
 customer_distribution = [1] * (60 * 8)
 
 height = 30
-# numero casse self-scan (ognuna occupa 2) + 1 spazio + barriera (1) + 1 spazio +
-# numero casse standard (ognuna occupa 2) + 2 spazi + numero gruppi casse self (ognuna occupa 8) +
-# barriera (1) + larghezza entering zone
-width = number_cash_desk_self_scan * 2 + 3 + number_cash_desk * 2 + 1 + \
-        number_cash_desk_self_service_groups * 8 + 1 + entering_zone_width
 pixels_width = 1500
-pixels_height = 1500 / width * height
-grid = CanvasGrid(agent_portrayal, width,
-                  height, pixels_width, pixels_height)
-
-# Grafici e metriche
-chart = ChartModule([{"Label": "Total_customers",
-                      "Color": "Black"}],
-                    data_collector_name='datacollector')
-
-chart1 = ChartModule([{"Label": "Density_total",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart2 = ChartModule([{"Label": "Density_standard",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart3 = ChartModule([{"Label": "Density_self_scan",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart4 = ChartModule([{"Label": "Flow_total",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart5 = ChartModule([{"Label": "Flow_standard",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart6 = ChartModule([{"Label": "Flow_self_scan",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart7 = ChartModule([{"Label": "Total_steps",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart8 = ChartModule([{"Label": "Avg_waiting_times_standard",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
-
-chart9 = ChartModule([{"Label": "Avg_waiting_times_self_scan",
-                       "Color": "Black"}],
-                     data_collector_name='datacollector')
+pixels_height = 1500
+grid = init_grid(height, pixels_width, pixels_height, zones_metadata)
 
 # Create server
 customer_legend_element = CustomerLegendElement()
 cash_desk_legend_element = CashDeskLegendElement()
 server = ModularServer(Supermarket,
-                       [grid, customer_legend_element, cash_desk_legend_element, chart, chart1, chart2, chart3,
-                        chart4, chart5, chart6, chart7, chart8, chart9],
+                       [grid, customer_legend_element, cash_desk_legend_element] + charts[:N_CHARTS],
                        "Supermarket",
                        {"zones_metadata": zones_metadata,
                         # lista con numero di clienti da generare ad ogni step es. [1, 2, 3, 2, 4, 5, 2, ...]
--- src/utility.py
+++ src/utility.py
@@ -2,6 +2,8 @@ import logging
 from datetime import datetime
 import os
 
+from mesa.visualization.modules import ChartModule, CanvasGrid
+
 from config import *
 
 
@@ -53,3 +55,91 @@ def agent_portrayal(agent):
                      "h": 1,
                      "Layer": 1}
     return portrayal
+
+
+def init_charts():
+    charts = []
+    chart0 = ChartModule([{"Label": "Total_customers",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart0)
+
+    chart1 = ChartModule([{"Label": "Density_total",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart1)
+
+    chart2 = ChartModule([{"Label": "Density_standard",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart2)
+
+    chart3 = ChartModule([{"Label": "Density_self_scan",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart3)
+
+    chart4 = ChartModule([{"Label": "Flow_total",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart4)
+
+    chart5 = ChartModule([{"Label": "Flow_standard",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart5)
+
+    chart6 = ChartModule([{"Label": "Flow_self_scan",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart6)
+
+    chart7 = ChartModule([{"Label": "Total_steps",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart7)
+
+    chart8 = ChartModule([{"Label": "Avg_waiting_times_standard",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart8)
+
+    chart9 = ChartModule([{"Label": "Avg_waiting_times_self_scan",
+                           "Color": "Black"}],
+                         data_collector_name='datacollector')
+    charts.append(chart9)
+
+    return charts
+
+
+def init_grid(height, pixels_width, pixels_height, zones_metadata):
+    # Unzip metadata
+    zones_metadata_dict = dict(zones_metadata)
+    number_cash_desk_self_scan = zones_metadata_dict['CASH_DESK_SELF_SCAN']
+    number_cash_desk = zones_metadata_dict['CASH_DESK_STANDARD']
+    number_cash_desk_self_service_groups = zones_metadata_dict['CASH_DESK_SELF_SERVICE']
+    entering_zone_width = zones_metadata_dict['ENTERING']
+
+    # numero casse self-scan (ognuna occupa 2) + 1 spazio + barriera (1) + 1 spazio +
+    # numero casse standard (ognuna occupa 2) + 2 spazi + numero gruppi casse self (ognuna occupa 8) +
+    # barriera (1) + larghezza entering zone
+    width = number_cash_desk_self_scan * 2 + 3 + number_cash_desk * 2 + 1 + \
+            number_cash_desk_self_service_groups * 8 + 1 + entering_zone_width
+
+    pixels_height = pixels_height / width * height
+
+    grid = CanvasGrid(agent_portrayal, width, height, pixels_width, pixels_height)
+
+    return grid
+
+
+def init_zones_metadata(entering_zone_width, shopping_zone_height, number_cash_desk_self_scan,
+                        number_cash_desk, shared_queue, number_cash_desk_self_service_groups):
+    zones_metadata = [('ENTERING', entering_zone_width),
+                      ('SHOPPING', shopping_zone_height),
+                      ('CASH_DESK_SELF_SCAN', number_cash_desk_self_scan),
+                      (('CASH_DESK_STANDARD_SHARED_QUEUE', number_cash_desk) if shared_queue else
+                       ('CASH_DESK_STANDARD', number_cash_desk)),
+                      ('CASH_DESK_SELF_SERVICE', number_cash_desk_self_service_groups),
+                      ('CASH_DESK_RESERVED', 1)]
+    return zones_metadata
